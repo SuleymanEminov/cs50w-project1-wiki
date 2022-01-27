b37e6c27cd1a692df6dfd73844ad0599c7f06e5f
Suleyman <suleymaneminov22@gmail.com>
2022-01-27 18:51:40 -0500

    index.html spec done, search spec started

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = "encyclopedia"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:entry_title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from . import util
 
@@ -8,35 +8,78 @@ from django.urls import reverse
 from markdown2 import Markdown
 
 
+class SearchForm(forms.Form):
+    """Class for Search form"""
+    title = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "search",
+        "placeholder": "Search Encyclopedia"
+    }))
+
+
 def index(request):
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "search_form": SearchForm()
     })
 
-def entry(request, entry_title):
+
+
+def entry(request, title):
     """
     This is a getter function that gets user input from index.html forms,
     and returns back a converted entry if it exsists in the entries directory
     """
 
-    if request.method == "POST":
+    entry_file = util.get_entry(title)
+    print(entry_file)
 
-        entry_title = request.POST['q']
-
-
-    if (util.get_entry(entry_title) is not None):
+    if entry_file is not None: # entry exsists in the directory
         markdowner = Markdown() # This is to convert Markdown to HTML
 
-        entry = util.get_entry(entry_title) # get 
-
-        converted_data = markdowner.convert(entry) 
-        
-        return render(request, "encyclopedia/display.html", {
-        "title": entry_title,
-        "entry": converted_data,
+        converted_data = markdowner.convert(entry_file)
+ 
+            
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": converted_data,
+            "search_form": SearchForm()
     })
-    else:
+    else: # entry doesn't exsist
         return render(request, "encyclopedia/404.html", {
-            "title": entry_title
+            "title": title,
+            "search_form": SearchForm()
         })
+
+
+def search(request):
+    """
+    This function is a search function that listens to the search form 
+    and returns searched article if exsists in the 'entries' directory
+    """
+    print("here")
+
+
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            entry_file = util.get_entry(title)
+
+
+            if entry_file:
+                # If entry exsists in the entries directory,
+                # redirect to that article 
+                return redirect(reverse("entry",args=[title]))
+            else: #page doesn't exsist, call search page
+                return render(request, "encyclopedia/search.html", {
+                "title": title,
+                "search_form": SearchForm()
+            })
+
+    else:
+        return redirect(reverse('index'))
+
+
 
